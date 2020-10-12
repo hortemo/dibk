b1adb80e1422088223176682c7cc2212721e36ab
Henrik Hortemo <henrikhortemo@icloud.com>
2020-10-12 13:54:12 +0200

    Simplified using json2csv options

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const request = require('request-promise');
-const json2csv = require('json2csv');
-const fs = require('fs').promises
+const request = require("request-promise");
+const json2csv = require("json2csv");
+const fs = require("fs").promises;
 
 request
-	.get({
-		url: 'https://sgregister.dibk.no/api/enterprises.json',
-		json: true,
-		headers: {
-			'Accept': 'application/vnd.sgpub.v2'
-		}
-	})
-	.then(data => data.enterprises)
-	.then(enterprises => enterprises.reduce((acc, enterprise) => [
-		...acc,
-		...enterprise.valid_approval_areas.map(valid_approval_area => ({
-			organizational_number: enterprise.organizational_number,
-			name: enterprise.name,
-			function: valid_approval_area.function,
-			subject_area: valid_approval_area.subject_area,
-			pbl: valid_approval_area.pbl,
-			grade: valid_approval_area.grade
-		}))
-	], []))
-	.then(valid_approval_areas => json2csv.parse(valid_approval_areas))
-	.then(csv_output => fs.writeFile('./valid_approval_areas.csv', csv_output))
-
-
+  .get({
+    url: "https://sgregister.dibk.no/api/enterprises.json",
+    json: true,
+    headers: {
+      Accept: "application/vnd.sgpub.v2",
+    },
+  })
+  .then((data) => data.enterprises)
+  .then((enterprises) =>
+    json2csv.parse(enterprises, {
+      fields: [
+        "organizational_number",
+        "name",
+        "businessaddress.postal_code",
+        "businessaddress.postal_town",
+        "valid_approval_areas.subject_area",
+        "valid_approval_areas.function",
+        "valid_approval_areas.pbl",
+        "valid_approval_areas.grade",
+      ],
+      transforms: [
+        json2csv.transforms.unwind({ paths: ["valid_approval_areas"] }),
+        json2csv.transforms.flatten({ separator: "." }),
+      ],
+    })
+  )
+  .then((csv_output) => fs.writeFile("./enterprises.csv", csv_output));
